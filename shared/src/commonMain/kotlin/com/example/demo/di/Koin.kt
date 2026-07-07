6ab4615039c6b10206e0bb1274545b3ac98f4eac
saurabh kaipurkar <123494114+saurabhkaipurkar@users.noreply.github.com>
2026-07-07 13:07:58 +0530

    Refactor initKoin to use function overloads instead of default parameters

--- shared/src/commonMain/kotlin/com/example/demo/di/Koin.kt
+++ shared/src/commonMain/kotlin/com/example/demo/di/Koin.kt
@@ -3,7 +3,11 @@ package com.example.demo.di
 import org.koin.core.context.startKoin
 import org.koin.dsl.KoinAppDeclaration
 
-fun initKoin(appDeclaration: KoinAppDeclaration = {}) {
+fun initKoin() {
+    initKoin { }
+}
+
+fun initKoin(appDeclaration: KoinAppDeclaration) {
 
     startKoin {
         appDeclaration()
